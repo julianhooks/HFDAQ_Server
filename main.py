e2a0eed2512577e95398e54287d51a60e2b79c94
julianhooks <julian.delb.hooks@gmail.com>
2026-04-15 22:01:52 -0400

    Project Description

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def main() -> None:
-    print("Hello World")
+    # This program needs to do a few things
+    # [TO-DO] Initialize a modbus tcp server on port 502
+    # [TO-DO] Expose the following:
+        # An "IsRecording" coil
+    # [TO-DO] Map an IsRecording write event to toggle data recording
+    # [TO-DO] Write data recording function
+        # - For labjack in streaming mode
+        # - For USB XLR audio interface
+    # [TO-DO] add sftp server to machine for data access (Not through this repo)
     return
 
 if (__name__ == "__main__"):
